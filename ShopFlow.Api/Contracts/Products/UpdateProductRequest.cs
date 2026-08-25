04a8bf933c5d9a3bf888bb59f29a04ab41920b8a
Prem <mprem39@gmail.com>
2026-08-25 20:35:54 +0530

    feat: add product CRUD endpoints

---
+++ ShopFlow.Api/Contracts/Products/UpdateProductRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace ShopFlow.Api.Contracts.Products;
+
+public sealed record UpdateProductRequest(
+    string Name,
+    decimal Price);
--- ShopFlow.Api/Controllers/ProductController.cs
+++ ShopFlow.Api/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using ShopFlow.Api.Contracts.Products;
+using ShopFlow.Api.Models;
 
 namespace ShopFlow.Api.Controllers;
 
@@ -6,17 +8,17 @@ namespace ShopFlow.Api.Controllers;
 [Route("api/products")]
 public class ProductsController : ControllerBase
 {
-    private readonly Product[] products = new[]
+    private static readonly List<Product> _products = new List<Product>
         {
             new Product
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("73cc57fb-d9ad-4be9-a01f-1d7a48ab1c48"),
                 Name = "Mechanical Keyboard",
                 Price = 4999
             },
             new Product
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("73cc57fb-d9ad-4be9-a01f-1d7a48ab1c49"),
                 Name = "Wireless Mouse",
                 Price = 2499
             }
@@ -25,25 +27,89 @@ public class ProductsController : ControllerBase
     [HttpGet]
     public IActionResult GetProducts()
     {
-        
+        var products = _products
+            .Select(product => new ProductResponse(
+                product.Id,
+                product.Name,
+                product.Price))
+            .ToArray();
+
         return Ok(products);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetProduct(Guid id)
     {
-        var product = products.FirstOrDefault(p => p.Id == id);
+        var product = _products.FirstOrDefault(p => p.Id == id);
         if (product is null)
         {
             return NotFound();
         }
-        return Ok(product);
+       var response = new ProductResponse(
+       product.Id,
+       product.Name,
+       product.Price);
+        return Ok(response);
     }
 
-    private sealed class Product
+    [HttpPost]
+    public IActionResult CreateProduct(CreateProductRequest request)
     {
-        public Guid Id { get; init; }
-        public string Name { get; init; } = string.Empty;
-        public int Price { get; init; }
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Price = request.Price
+        };
+        _products.Add(product);
+        // For now, we're not persisting it.
+
+        var response = new ProductResponse(
+            product.Id,
+            product.Name,
+            product.Price);
+
+        return CreatedAtAction(
+            nameof(GetProduct),
+            new { id = product.Id },
+            response);
     }
+
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpdateProduct(Guid id,UpdateProductRequest updateProductRequest)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        _products.Remove(product);
+
+        var updateProduct = new Product
+        {
+            Id = id,
+            Name = updateProductRequest.Name,
+            Price = updateProductRequest.Price
+        };
+
+        // For now, we're not persisting it.
+        _products.Add(updateProduct);
+       
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteProduct(Guid id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        _products.Remove(product);
+        return NoContent();
+    }
+
+
 }
